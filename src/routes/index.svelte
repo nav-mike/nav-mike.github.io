40f09d54bff5a9fbe1a69e28d0230a804d348da2
Mikhail Navrotskii <m.navrotskiy@gmail.com>
2022-05-27 13:29:52 +0300

    add base skeleton of the page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,60 @@
-<h1 class="text-3xl font-bold underline">
-  Hello World!
-</h1>
+<div class="flex flex-row">
+  <div class="flex flex-col">
+    <img class="h-40 max-h-40 w-40" src="https://avatars.githubusercontent.com/u/2811491?v=4" alt="Mikhail Navrotskii" />
+    <h1>Mikhail Navrotskii</h1>
+    <p>Backend developer in <a href="https://www.brella.io" target="_blank">brella.io</a></p>
+    <p>Living in Espoo, Finland</p>
+    <p>Languages: Rus, Eng</p>
+    <div>Contacts & links:</div>
+    <ul>
+      <li>Email</li>
+      <li>GitHub</li>
+      <li>LinkedIn</li>
+      <li>Twitter</li>
+      <li>Facebook</li>
+      <li>Telegram</li>
+    </ul>
+  </div>
+  <div>details info:
+    <!-- Tabs: -->
+    <div>My professional skills:
+    <ul>
+      <li>Ruby</li>
+      <li>Ruby on Rails</li>
+      <li>PostgreSQL</li>
+      <li>GraphQL</li>
+      <li>RSpec</li>
+      <li>JavaScript</li>
+      <li>Firestore</li>
+      <li>TypeScript</li>
+      <li>React.js</li>
+      <li>React Native</li>
+      <li>Nodejs</li>
+      <li>NestJS</li>
+      <li>Tailwind</li>
+      <li>Svelte</li>
+      <li>Elixir</li>
+      <li>Phoenix</li>
+      <li>Git</li>
+      <li>Docker</li>
+      <li>Redis</li>
+      <li>Go</li>
+    </ul></div>
+    <div>My professional experience:
+    <ul>
+      <li>Backend Developer (brella.io) | Aug 2021 - Present | Helsinki, Finland</li>
+      <li>Backend Developer (zeustrack.io) | Apr 2015 - Jul 2021 | Espoo, Finland | Spb, Russia</li>
+      <li>Research Engineer (ifmo.ru) | Sep 2015 - Aug 2016 | Spb, Russia</li>
+      <li>Fullstack Developer (mirturbaz.ru) | Feb 2014 - Aug 2014 | Volgograd, Russia</li>
+      <li>Web Developer, Internship (intervolga.ru) | Jan 2013 - Apr 2013 | Volgograd, Russia</li>
+    </ul></div>
+    <div>My education:
+      <ul>
+        <li>IFMO University | CAD | 2014 - 2018</li>
+        <li>Volgograd State Technical University | MS Software Engineering | 2012 - 2014</li>
+        <li>Volgograd State Technical University | BS Software Development | 2008 - 2012</li>
+      </ul>
+    </div>
+    <div>My Projects</div>
+  </div>
+</div>
