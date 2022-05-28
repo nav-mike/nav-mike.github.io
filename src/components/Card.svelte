b2f5e98b2f121377299536a7a27000197bc2ae26
Mikhail Navrotskii <m.navrotskiy@gmail.com>
2022-05-28 13:39:57 +0300

    add card component (for job exp and edu)

---
+++ src/components/Card.svelte
@@ -0,0 +1,21 @@
+<script>
+	export let imageUrl;
+	export let position;
+	export let company;
+	export let companyUrl;
+	export let period;
+	export let location;
+</script>
+
+<div class="flex m-2">
+	<img src={imageUrl} alt={company} class="h-20 w-20 m-5" />
+	<div>
+		<h3 class="font-bold">{position}</h3>
+		<h4 class="hover:underline">
+			<a href={companyUrl} target="_blank">{company}</a>
+		</h4>
+		<h5 class="font-light text-gray-500">{period}</h5>
+		<h5 class="font-light text-gray-500">{location}</h5>
+	</div>
+</div>
+<hr />
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,60 +1,146 @@
+<script>
+	import Card from '../components/Card.svelte';
+</script>
+
 <div class="flex flex-row">
-  <div class="flex flex-col">
-    <img class="h-40 max-h-40 w-40" src="https://avatars.githubusercontent.com/u/2811491?v=4" alt="Mikhail Navrotskii" />
-    <h1>Mikhail Navrotskii</h1>
-    <p>Backend developer in <a href="https://www.brella.io" target="_blank">brella.io</a></p>
-    <p>Living in Espoo, Finland</p>
-    <p>Languages: Rus, Eng</p>
-    <div>Contacts & links:</div>
-    <ul>
-      <li>Email</li>
-      <li>GitHub</li>
-      <li>LinkedIn</li>
-      <li>Twitter</li>
-      <li>Facebook</li>
-      <li>Telegram</li>
-    </ul>
-  </div>
-  <div>details info:
-    <!-- Tabs: -->
-    <div>My professional skills:
-    <ul>
-      <li>Ruby</li>
-      <li>Ruby on Rails</li>
-      <li>PostgreSQL</li>
-      <li>GraphQL</li>
-      <li>RSpec</li>
-      <li>JavaScript</li>
-      <li>Firestore</li>
-      <li>TypeScript</li>
-      <li>React.js</li>
-      <li>React Native</li>
-      <li>Nodejs</li>
-      <li>NestJS</li>
-      <li>Tailwind</li>
-      <li>Svelte</li>
-      <li>Elixir</li>
-      <li>Phoenix</li>
-      <li>Git</li>
-      <li>Docker</li>
-      <li>Redis</li>
-      <li>Go</li>
-    </ul></div>
-    <div>My professional experience:
-    <ul>
-      <li>Backend Developer (brella.io) | Aug 2021 - Present | Helsinki, Finland</li>
-      <li>Backend Developer (zeustrack.io) | Apr 2015 - Jul 2021 | Espoo, Finland | Spb, Russia</li>
-      <li>Research Engineer (ifmo.ru) | Sep 2015 - Aug 2016 | Spb, Russia</li>
-      <li>Fullstack Developer (mirturbaz.ru) | Feb 2014 - Aug 2014 | Volgograd, Russia</li>
-      <li>Web Developer, Internship (intervolga.ru) | Jan 2013 - Apr 2013 | Volgograd, Russia</li>
-    </ul></div>
-    <div>My education:
-      <ul>
-        <li>IFMO University | CAD | 2014 - 2018</li>
-        <li>Volgograd State Technical University | MS Software Engineering | 2012 - 2014</li>
-        <li>Volgograd State Technical University | BS Software Development | 2008 - 2012</li>
-      </ul>
-    </div>
-    <div>My Projects</div>
-  </div>
+	<div class="flex flex-col">
+		<img
+			class="h-40 max-h-40 w-40"
+			src="https://avatars.githubusercontent.com/u/2811491?v=4"
+			alt="Mikhail Navrotskii"
+		/>
+		<h1>Mikhail Navrotskii</h1>
+		<p>Backend developer in <a href="https://www.brella.io" target="_blank">brella.io</a></p>
+		<p>Living in Espoo, Finland</p>
+		<p>Languages: Rus, Eng</p>
+		<div>Contacts & links:</div>
+		<ul>
+			<li>Email</li>
+			<li>GitHub</li>
+			<li>LinkedIn</li>
+			<li>Twitter</li>
+			<li>Facebook</li>
+			<li>Telegram</li>
+		</ul>
+	</div>
+	<div>
+		details info:
+		<!-- Tabs: -->
+		<div>
+			My professional skills:
+			<ul>
+				<li>Ruby</li>
+				<li>Ruby on Rails</li>
+				<li>PostgreSQL</li>
+				<li>GraphQL</li>
+				<li>RSpec</li>
+				<li>JavaScript</li>
+				<li>Firestore</li>
+				<li>TypeScript</li>
+				<li>React.js</li>
+				<li>React Native</li>
+				<li>Nodejs</li>
+				<li>NestJS</li>
+				<li>Tailwind</li>
+				<li>Svelte</li>
+				<li>Elixir</li>
+				<li>Phoenix</li>
+				<li>Git</li>
+				<li>Docker</li>
+				<li>Redis</li>
+				<li>Go</li>
+			</ul>
+		</div>
+		<div>
+			My professional experience:
+			<ul>
+				<li>
+					<Card
+						imageUrl="https://avatars.githubusercontent.com/u/23238202?s=200&v=4"
+						position="Backend Developer"
+						company="Brella"
+						companyUrl="https://www.brella.io"
+						period="Aug 2021 - Present"
+						location="Helsinki, Finland"
+					/>
+				</li>
+				<li>
+					<Card
+						imageUrl="https://zeustrack.io/assets-new/img/logo.svg"
+						position="Backend Developer"
+						company="Zeustrack"
+						companyUrl="https://www.zeustrack.io"
+						period="Apr 2015 - Jul 2021"
+						location="Espoo, Finland; Spb, Russia"
+					/>
+				</li>
+				<li>
+					<Card
+						imageUrl="https://habrastorage.org/getpro/moikrug/uploads/university/000/006/855/logo/a25509c024193f20603c116dd17ff222.png"
+						position="Research Engineer"
+						company="ITMO University"
+						companyUrl="https://en.itmo.ru"
+						period="Sep 2015 - Aug 2016"
+						location="Spb, Russia"
+					/>
+				</li>
+				<li>
+					<Card
+						imageUrl="https://habrastorage.org/getpro/moikrug/uploads/company/100/004/429/0/logo/medium_515d269ea035141258c1bd4e551ba8bf.jpeg"
+						position="Fullstack Developer"
+						company="MirTurbaz"
+						companyUrl="https://mirturbaz.ru/"
+						period="Feb 2014 - Aug 2014"
+						location="Volgograd, Russia"
+					/>
+				</li>
+				<li>
+					<Card
+						imageUrl="https://habrastorage.org/getpro/moikrug/uploads/company/100/006/038/4/logo/medium_53de6c80c21767a10d3d5a5a8be650cd.png"
+						position="Web Developer, Internship"
+						company="Intervolga"
+						companyUrl="https://www.intervolga.ru/"
+						period="Jan 2013 - Apr 2013"
+						location="Volgograd, Russia"
+					/>
+				</li>
+			</ul>
+		</div>
+		<div>
+			My education:
+			<ul>
+				<li>
+					<Card
+						imageUrl="https://habrastorage.org/getpro/moikrug/uploads/university/000/006/855/logo/a25509c024193f20603c116dd17ff222.png"
+						position="CAD"
+						company="ITMO University"
+						companyUrl="https://en.itmo.ru"
+						period="2014 - 2018"
+						location="Spb, Russia"
+					/>
+				</li>
+				<li>
+					<Card
+						imageUrl="https://habrastorage.org/getpro/moikrug/uploads/university/000/001/753/logo/7aee3553420f19c0beedcb22b38cd9b4.jpeg"
+						position="Software Engineering (MS)"
+						company="Volgograd State Technical University"
+						companyUrl="https://www.vstu.ru/eng/"
+						period="2012 - 2014"
+						location="Volgograd, Russia"
+					/>
+				</li>
+				<li>
+					<Card
+						imageUrl="https://habrastorage.org/getpro/moikrug/uploads/university/000/001/753/logo/7aee3553420f19c0beedcb22b38cd9b4.jpeg"
+						position="Software Development (BS)"
+						company="Volgograd State Technical University"
+						companyUrl="https://www.vstu.ru/eng/"
+						period="2008 - 2012"
+						location="Volgograd, Russia"
+					/>
+				</li>
+			</ul>
+		</div>
+		<div>My Projects</div>
+	</div>
 </div>
