b9bbff3417c8751f413565dc67fec7672f793844
Mikhail Navrotskii <m.navrotskiy@gmail.com>
2022-05-30 18:45:29 +0300

    add more components

---
+++ src/components/Contacts.svelte
@@ -0,0 +1,46 @@
+<script>
+	import BaseLink from './BaseLink.svelte';
+	import Wall from './Wall.svelte';
+</script>
+
+<div class="m-5">
+	<Wall title="Contacts & links">
+		<span>
+			<BaseLink
+				link="mailto:m.navrotskiy@gmail.com"
+				label="m.navrotskiy@gmail.com"
+				icon="fa-regular fa-envelope"
+			/>
+		</span>
+		<span>
+			<BaseLink link="https://github.com/nav-mike" label="nav-mike" icon="fa-brands fa-github" />
+		</span>
+		<span>
+			<BaseLink
+				link="www.linkedin.com/in/mikhail-navrotskii-8857a041"
+				label="Mikhail Navrotskii"
+				icon="fa-brands fa-linkedin"
+			/>
+		</span>
+		<span>
+			<BaseLink
+				link="https://twitter.com/m_navrotskiy"
+				label="@m_navrotskiy"
+				icon="fa-brands fa-twitter"
+			/>
+		</span>
+		<span>
+			<BaseLink
+				link="https://www.facebook.com/mnavrotskiy"
+				label="@mnavrotskiy"
+				icon="fa-brands fa-facebook"
+			/>
+		</span>
+		<span>
+			<div class="flex items-center m-2">
+				<i class="fa-brands fa-telegram m-2" />
+				<span>@navmike</span>
+			</div>
+		</span>
+	</Wall>
+</div>
---
+++ src/components/Education.svelte
@@ -0,0 +1,39 @@
+<script>
+	import Card from './Card.svelte';
+	import Wall from './Wall.svelte';
+</script>
+
+<div class="m-2">
+	<Wall title="My education">
+		<span>
+			<Card
+				imageUrl="https://habrastorage.org/getpro/moikrug/uploads/university/000/006/855/logo/a25509c024193f20603c116dd17ff222.png"
+				position="CAD"
+				company="ITMO University"
+				companyUrl="https://en.itmo.ru"
+				period="2014 - 2018"
+				location="Spb, Russia"
+			/>
+		</span>
+		<span>
+			<Card
+				imageUrl="https://habrastorage.org/getpro/moikrug/uploads/university/000/001/753/logo/7aee3553420f19c0beedcb22b38cd9b4.jpeg"
+				position="Software Engineering (MS)"
+				company="Volgograd State Technical University"
+				companyUrl="https://www.vstu.ru/eng/"
+				period="2012 - 2014"
+				location="Volgograd, Russia"
+			/>
+		</span>
+		<span>
+			<Card
+				imageUrl="https://habrastorage.org/getpro/moikrug/uploads/university/000/001/753/logo/7aee3553420f19c0beedcb22b38cd9b4.jpeg"
+				position="Software Development (BS)"
+				company="Volgograd State Technical University"
+				companyUrl="https://www.vstu.ru/eng/"
+				period="2008 - 2012"
+				location="Volgograd, Russia"
+			/>
+		</span>
+	</Wall>
+</div>
