bcf04be5abe840c62b7998efc8c4247ab6303cf0
Mikhail Navrotskii <m.navrotskiy@gmail.com>
2022-05-31 15:32:18 +0300

    add shadow

--- src/components/Wall.svelte
+++ src/components/Wall.svelte
@@ -3,7 +3,7 @@
 	export let klasses = [];
 </script>
 
-<div class="bg-slate-200 p-5 rounded-md">
+<div class="bg-slate-200 p-5 rounded-md drop-shadow-lg">
 	<h3 class="text-2xl m-2">{title}</h3>
 	<div class="{klasses} m-2"><slot /></div>
 </div>
