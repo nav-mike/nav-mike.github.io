1751d1b77a02eccbe8a0c9fa67d09ad318738fe2
Mikhail Navrotskii <mikhail.navrotskii@zevoy.com>
2023-08-14 16:01:54 +0300

    add basic link component

---
+++ src/components/BaseLink.svelte
@@ -0,0 +1,25 @@
+<script>
+  export let link;
+  export let label;
+  export let icon;
+
+  let hover = "";
+
+  const mouseEnterHandler = () => {
+    hover = "hover";
+  };
+
+  const mouseLeaveHandler = () => {
+    hover = "";
+  };
+</script>
+
+<a
+  href={link}
+  class="flex items-center m-2"
+  on:mouseenter={mouseEnterHandler}
+  on:mouseleave={mouseLeaveHandler}
+>
+  <i class="{icon} m-2" />
+  <span class={hover}>{label}</span>
+</a>
